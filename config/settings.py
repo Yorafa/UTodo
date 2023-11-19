fc94653c6601deb9d668ea032184cbfb336d4b9b
Yorafa <thomasyorafa@gmail.com>
2023-11-19 18:27:18 -0500

    try fix

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 import dj_database_url
 
-DATABASES = {'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
+DATABASES = {'default' : dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600, ssl_require=True)}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
