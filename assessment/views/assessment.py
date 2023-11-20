5c154efee7bff7b150b0985ca0288a53a5bdc94f
Yorafa <thomasyorafa@gmail.com>
2023-11-19 19:46:40 -0500

    update

--- assessment/views/assessment.py
+++ assessment/views/assessment.py
@@ -1,23 +1,33 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from assessment.models import Assessment
 from assessment.serializers.assessment import AssessmentSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import permissions
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD, or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed if the user is the owner of the assessment.
+        return obj.course.user == request.user
+
 
 class AssessmentView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AssessmentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
         assessment_id = self.kwargs['pk']
-        assessment = Assessment.objects.get(id=assessment_id)
-
-        if assessment.course.user == user:
-            return Assessment.objects.filter(id=assessment_id)
-        else:
-            return Assessment.objects.none()
+        return Assessment.objects.get(id=assessment_id)
     
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
