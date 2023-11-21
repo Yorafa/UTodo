b9c81b1b28342791bc12b03acfb79a22bf1c459e
Yorafa <thomasyorafa@gmail.com>
2023-11-20 20:59:28 -0500

    update main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,7 +5,8 @@ function MainPage() {
     document.title = 'Dashboard';
     return (
         <div>
-            <p>Feature future.</p>
+            <p>Secure Key public so do not register with your faviroute password</p>
+            <p>More features coming future</p>
         </div>
     );
 }
